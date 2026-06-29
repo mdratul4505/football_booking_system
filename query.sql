c71ef3f95da34dbedbd09ad1d877ea66eb9a7434
mdratul4505 <dev.raul.coder@gmail.com>
2026-06-29 21:39:18 +0600

    added my third query

--- query.sql
+++ query.sql
@@ -13,3 +13,14 @@ FROM Users
 WHERE full_name LIKE 'Tanvir%'
    OR full_name ILIKE '%haque%';
 
+
+-- query 3-----------
+SELECT 
+    booking_id, 
+    user_id, 
+    match_id, 
+    COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
+
+
