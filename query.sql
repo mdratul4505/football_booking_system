c535651307a3ab96e3705b90748e45ecc0d5905c
mdratul4505 <dev.raul.coder@gmail.com>
2026-06-29 21:38:28 +0600

    added my second query

--- query.sql
+++ query.sql
@@ -6,3 +6,10 @@ FROM Matches
 WHERE tournament_category = 'Champions League'
 AND match_status = 'Available';
 
+---    query 2
+
+SELECT user_id, full_name, email
+FROM Users
+WHERE full_name LIKE 'Tanvir%'
+   OR full_name ILIKE '%haque%';
+
