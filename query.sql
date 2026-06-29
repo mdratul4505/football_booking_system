f02fa9257e363f64f6e5780d39478fe852ddb1cb
mdratul4505 <dev.raul.coder@gmail.com>
2026-06-29 21:40:01 +0600

    added my 4th query

--- query.sql
+++ query.sql
@@ -24,3 +24,15 @@ FROM Bookings
 WHERE payment_status IS NULL;
 
 
+ -- query 4-------------
+SELECT 
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
+
+
+
